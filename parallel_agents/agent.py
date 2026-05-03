dc72e3b149c48e1385fa3247d0e112c5514ec8d3
pyda-lohith <lohi.pyda@gmail.com>
2026-05-03 21:55:00 +0530

    parallel-agent

---
+++ parallel_agents/agent.py
@@ -0,0 +1,7 @@
+import datetime
+from typing import Any, Dict, List, Optional
+from google.adk.agents import Agent, SequentialAgent
+from google.adk.tools import google_search
+from google.adk.agents import Agent, ParallelAgent
+
+
